4cec96dcfd7ed27d7b13c51dd5aa98447ee4fc2e
Collins Makui <52255997+Makcollins@users.noreply.github.com>
2024-10-29 19:23:05 +0300

    Update JobDetails.jsx

--- client-job/src/pages/JobDetails.jsx
+++ client-job/src/pages/JobDetails.jsx
@@ -6,7 +6,7 @@ const JobDetails = () => {
     const {id} = useParams();
     const [job, setJob] = useState([]) 
     useEffect(()=> {
-        fetch(`http://localhost:3000/all-jobs/${id}`)
+        fetch(`https://jobportal-backend-waol.onrender.com/all-jobs/${id}`)
         .then(res => res.json())
         .then(data => setJob(data))
     },[])
