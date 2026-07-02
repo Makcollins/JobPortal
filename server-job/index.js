5bf3cd3a8de2b69873d2591c1654fc8518c2e1bb
Makcollins <makcollins16@gmail.com>
2026-07-02 21:50:22 +0300

    introduction of serpapi

--- server-job/index.js
+++ server-job/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const cors =require('cors')
+const cors = require('cors')
 require('dotenv').config()
 // const connection = require("./db");
 const port = process.env.PORT || 3000
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 
+//GOOGLE JOBS API
+const { getJson } = require("serpapi");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@job-portal.grpehgs.mongodb.net/?appName=job-portal`;
@@ -22,92 +24,110 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
 
-    //create db
-    const db = client.db("mernJobPortal");
-    const jobCollections = db.collection("myJobs")
+//     //create db
+//     const db = client.db("mernJobPortal");
+//     const jobCollections = db.collection("myJobs")
 
-    //post a job
-    app.post("/post-job", async(req,res) =>{
-      const body=req.body;
-      body.createAt = new Date();
-      const result= await jobCollections.insertOne(body);
+//     //post a job
+//     app.post("/post-job", async (req, res) => {
+//       const body = req.body;
+//       body.createAt = new Date();
+//       const result = await jobCollections.insertOne(body);
 
-      if(result.insertedId){
-        return res.status(200).send(result);
-      }else{
-        return res.status(404).send({
-          message: "can not insert try again later",
-          status: false
-        })
-      }
-    })
+//       if (result.insertedId) {
+//         return res.status(200).send(result);
+//       } else {
+//         return res.status(404).send({
+//           message: "can not insert try again later",
+//           status: false
+//         })
+//       }
+//     })
 
-    //get all jobs
-    app.get("/all-jobs", async(req,res) => {
-      const jobs =await jobCollections.find({}).toArray()
-      res.send(jobs);
-    })
+//     //get all jobs
+//     app.get("/all-jobs", async (req, res) => {
+//       // const jobs =await jobCollections.find({}).toArray()
+//       const jobs = [];
+//       res.send(jobs);
+//     })
 
-    //get a single job using id
-    app.get("/all-jobs/:id", async(req,res) => {
-      const id = req.params.id;
-      const job = await jobCollections.findOne({
-        _id: new ObjectId(id)
-      })
-      res.send(job)
-    })
+//     //get a single job using id
+//     app.get("/all-jobs/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const job = await jobCollections.findOne({
+//         _id: new ObjectId(id)
+//       })
+//       res.send(job)
+//     })
 
-    //get jobs by email
-    app.get("/myJobs/:email", async(req,res) => {
-      // console.log(req.params.email)
-      const jobs = await jobCollections.find({postedBy :req.params.email }).toArray();
-      res.send(jobs)
-    })
+//     //get jobs by email
+//     app.get("/myJobs/:email", async (req, res) => {
+//       // console.log(req.params.email)
+//       const jobs = await jobCollections.find({ postedBy: req.params.email }).toArray();
+//       res.send(jobs)
+//     })
 
-    //delete a job
-    app.delete('/job/:id', async(req,res) => {
-      const id=req.params.id;
-      const filter ={_id: new ObjectId(id)}
-      const result = await jobCollections.deleteOne(filter);
-      res.send(result);
-    })
+//     //delete a job
+//     app.delete('/job/:id', async (req, res) => {
+//       const id = req.params.id;
+//       const filter = { _id: new ObjectId(id) }
+//       const result = await jobCollections.deleteOne(filter);
+//       res.send(result);
+//     })
 
-    //update a job
-    app.patch("/update-job/:id", async(req,res) => {
-      const id = req.params.id;
-      const jobData = req.body;
-      const filter = {_id: new ObjectId}
-      const options = {upsert: true};
-      const updateDoc = {
-        $set: {
-          ...jobData
-        },
-      };
+//     //update a job
+//     app.patch("/update-job/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const jobData = req.body;
+//       const filter = { _id: new ObjectId }
+//       const options = { upsert: true };
+//       const updateDoc = {
+//         $set: {
+//           ...jobData
+//         },
+//       };
 
-      const result = await jobCollections.updateOne(filter,updateDoc,options);
-      res.send(result)
-    })
+//       const result = await jobCollections.updateOne(filter, updateDoc, options);
+//       res.send(result)
+//     })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
+app.get('/jobs', async (req, res) => {
+  let jobs = [];
+  await getJson({
+  engine: "google_jobs",
+  q: "Developer",
+  location: "",
+  // google_domain: "google.com",
+  // hl: "en",
+  // gl: "us",
+  api_key: "2b3baaae9fc5a40a39d0c108395b22a1cadd19985c98a7c9d715f66c7be939f5"
+}, (json) => {
+  jobs = json["jobs_results"];
+  // console.log(json["jobs_results"]);
+  
+});
+  res.send(jobs);
+})
 
 app.get('/', (req, res) => {
   res.send('Job portal API working!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
