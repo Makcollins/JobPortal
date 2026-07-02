112eab3ae3aa4972990f8a5279192dabf1b7ac3b
Makcollins <makcollins16@gmail.com>
2026-07-02 21:53:07 +0300

    front end too

--- client-job/src/components/Card.jsx
+++ client-job/src/components/Card.jsx
@@ -5,23 +5,23 @@ import { LuAlarmClock } from "react-icons/lu";
 import { PiCurrencyDollarBold } from "react-icons/pi";
 
 const Card = ({data}) => {
-    const {_id,companyName,companyLogo,jobTitle,salary,qualification,industry,jobLocation,employmentType,deadline,
+    const {job_id,company_name,thumbnail,title,salary,detected_extensions,location,
         description}= data;
   return (
     <section className='card hover:shadow-inner '>
-        <Link to={`/job/${_id}`} className='flex gap-4 flex-col sm:flex-row items-start '>
-            <img src={companyLogo} alt="" className='w-20 max-h-20' />        
+        <Link to={`/job/${job_id}`} className='flex gap-4 flex-col sm:flex-row items-start '>
+            <img src={thumbnail} alt={`${company_name} logo`} className='w-20 max-h-20' />        
             <div>
-                <h4 className='text-primary mb-1'>{companyName}</h4>
-                <h3 className='text-lg font-semibold mb-2'>{jobTitle}</h3>
+                <h4 className='text-primary mb-1'>{company_name}</h4>
+                <h3 className='text-lg font-semibold mb-2'>{title}</h3>
                 <div className='text-primary/70 text-base flex flex-wrap gap-2 mb-2'>
-                    <span className='flex items-center gap-2'><GrMap /> {jobLocation}</span>
-                    <span className='flex items-center gap-2'><LuAlarmClock /> {employmentType}</span>
-                    <span className='flex items-center gap-2'><PiCurrencyDollarBold/> {salary}k</span>
-                    <span className='flex items-center gap-2'><GrCalendar /> {deadline}</span>
+                    <span className='flex items-center gap-2'><GrMap /> {location}</span>
+                    <span className='flex items-center gap-2'><LuAlarmClock /> {detected_extensions.schedule_type}</span>
+                    <span className='flex items-center gap-1'><PiCurrencyDollarBold/>{salary || '_'} k</span>
+                    <span className='flex items-center gap-2'><GrCalendar /> {detected_extensions.posted_at}</span>
 
                 </div>
-                <p className='text-primary/70 text-base'>{description}</p>
+                <p className='text-primary/70 text-base line-clamp-4'>{description}</p>
             </div>
         </Link>
     </section>
--- client-job/src/pages/Home.jsx
+++ client-job/src/pages/Home.jsx
@@ -6,105 +6,116 @@ import Sidebar from '../sidebar/Sidebar';
 import Footer from '../components/Footer';
 
 const Home = () => {
-  const [categorySelect,setCategorySelect] =useState(null);
-  const[jobs, setJobs] = useState([]);
+  const [categorySelect, setCategorySelect] = useState(null);
+  const [jobs, setJobs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
 
-    //fetch data  
-  useEffect(()=>{
+  // fetch data  
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://jobportal-backend-waol.onrender.com/all-jobs").then(res => res.json()).then(data => {
+  //     // console.log(data);
+  //     setJobs(data);
+  //     setIsLoading(false);
+  //   })
+  // }, []);
+
+  useEffect(() => {
     setIsLoading(true);
-    fetch("https://jobportal-backend-waol.onrender.com/all-jobs").then(res => res.json()).then(data=>{
+    fetch("http://localhost:3000/jobs").then(res => res.json()).then(data => {
       // console.log(data);
       setJobs(data);
       setIsLoading(false);
     })
-  },[]);
+  }, []);
 
-    //handle input change
-  const [query,setQuery] = useState("");
-    const handleInputChange = (event) =>{
-        setQuery(event.target.value);
+
+  //handle input change
+  const [query, setQuery] = useState("");
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  }
+
+  // filter jobs by title
+  const filtereditems = jobs.filter((job) => job.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+
+  // Radio based button filtering
+  const handleChange = (event) => {
+    setCategorySelect(event.target.value);
+  }
+
+  //button filtering
+  const handleClick = (event) => {
+    setCategorySelect(event.target.value);
+  }
+
+  //calc th index range
+  const calculatePageRange = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return { startIndex, endIndex };
+  }
+
+  //next page function
+  const nextPage = () => {
+    if (currentPage < Math.ceil(filtereditems.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  //previous page funtion
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  const filteredData = (jobs, selected, query) => {
+    let filteredJobs = jobs;
+
+    //filtering input items
+    if (query) {
+      filteredJobs = filtereditems;
     }
 
-    // filter jobs by title
-    const filtereditems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-     
-    // Radio based button filtering
-     const handleChange = (event) =>{
-      setCategorySelect(event.target.value);
-     }
+    // category filtering
+    if (selected) {
+      filteredJobs = filteredJobs.filter(({ location, company_name,detected_extensions }) => (
+        location.toLowerCase() === selected.toLowerCase() ||
+        detected_extensions.qualifications.toLowerCase() === selected.toLowerCase() ||
+        detected_extensions.posted_at >= selected ||
+        company_name.toLowerCase() === selected.toLowerCase() ||
+        detected_extensions.schedule_type.toLowerCase() === selected.toLowerCase()
 
-     //button filtering
-     const handleClick = (event) =>{
-      setCategorySelect(event.target.value);
-     }
+      ));
+    }
 
-     //calc th index range
-     const calculatePageRange =() =>{
-      const startIndex = (currentPage - 1) * itemsPerPage;
-      const endIndex = startIndex + itemsPerPage;
-      return {startIndex,endIndex}; 
-     }
+    //slice the data based on current page 
+    const { startIndex, endIndex } = calculatePageRange();
+    filteredJobs = filteredJobs.slice(startIndex, endIndex);
 
-     //next page function
-     const nextPage = () =>{
-      if(currentPage < Math.ceil(filtereditems.length/itemsPerPage)){
-        setCurrentPage(currentPage+1);
-      }
-     }
-     //previous page funtion
-     const prevPage = () => {
-      if(currentPage > 1){
-        setCurrentPage(currentPage - 1)
-      }
-     }
+    return filteredJobs.map((data, i) => <Card key={i} data={data} />)
+  }
 
-     const filteredData = (jobs, selected, query)=>{
-      let filteredJobs = jobs;
+  const result = filteredData(jobs, categorySelect, query);
 
-      //filtering input items
-      if(query){
-        filteredJobs = filtereditems;
-      }
-
-      // category filtering
-      if(selected){
-        filteredJobs = filteredJobs.filter(({jobLocation, qualification, experienceLevel, industry,employmentType,deadline})=>(
-          jobLocation.toLowerCase() === selected.toLowerCase() || 
-          qualification.toLowerCase() === selected.toLowerCase() ||
-          deadline >= selected ||
-          experienceLevel.toLowerCase() === selected.toLowerCase() ||
-          industry.toLowerCase() === selected.toLowerCase() ||
-          employmentType.toLowerCase() === selected.toLowerCase()
-         
-        ));
-      }
-
-      //slice the data based on current page 
-      const {startIndex,endIndex} = calculatePageRange();
-      filteredJobs = filteredJobs.slice(startIndex, endIndex);
-
-      return filteredJobs.map((data,i)=> <Card key={i} data={data}/>)
-     }
-
-     const result = filteredData(jobs,categorySelect, query);
+  // 51011da31c7fff6d9edc54a0301ce5e3 api-key
 
   return (
     <div>
-      <Banner query={query} handleInputChange={handleInputChange} handleChange={handleChange}/>
+      <Banner query={query} handleInputChange={handleInputChange} handleChange={handleChange} />
 
       {/* main content */}
       <div className='bg-[#FAFAFA] md:grid grid-cols-3 gap-8 lg:px-24 px-4 py-12'>
 
-              {/*Left- side job cards */}
+        {/*Left- side job cards */}
         <div className='col-span-2 bg-white p-4 rounded'>
 
           {
-            isLoading ? (<p className='font-medium'>Loading...</p>) : result.length > 0 ? (<Jobs result={result}/>) :<>
-            <h3 className='text-lg font-bold mb-2'>{result.length} Jobs</h3>
-            <p>No data found</p>
+            isLoading ? (<p className='font-medium'>Loading...</p>) : result.length > 0 ? (<Jobs result={result} />) : <>
+              <h3 className='text-lg font-bold mb-2'>{result.length} Jobs</h3>
+              <p>No data found</p>
             </>
           }
 
@@ -112,22 +123,22 @@ const Home = () => {
           {
             result.length > 0 ? (
               <div className='flex justify-center mt-4 space-x-8'>
-                <button onClick={prevPage} className='hover:underline' disabled = {currentPage=== 1}>Previous</button>
-                <span className='mx-2'>Page {currentPage} of {Math.ceil(filtereditems.length/itemsPerPage)}</span>
-                <button onClick={nextPage} disabled = {currentPage === Math.ceil(filtereditems.length / 
-                itemsPerPage)} className='hover:underline'>Next</button>
+                <button onClick={prevPage} className='hover:underline' disabled={currentPage === 1}>Previous</button>
+                <span className='mx-2'>Page {currentPage} of {Math.ceil(filtereditems.length / itemsPerPage)}</span>
+                <button onClick={nextPage} disabled={currentPage === Math.ceil(filtereditems.length /
+                  itemsPerPage)} className='hover:underline'>Next</button>
               </div>
             ) : ""
           }
-          
+
         </div>
 
         {/* right side */}
         <div className='bg-white p-4  rounded'>
-          <Sidebar handleChange={handleChange} handleClick={handleClick}/>
+          <Sidebar handleChange={handleChange} handleClick={handleClick} />
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
